6ac0da4029a9de6c0bd04c81f493b69ec37635d2
unknown <toma-s.7698@hotmial.com>
2022-05-13 23:55:46 +0900

    test

--- components/huskytest.tsx
+++ components/huskytest.tsx
@@ -1,11 +1,11 @@
 const huskytest = () => {
-    return(
-        <>
-            <a href=""></a>
-            <h1>Husky Test</h1>
-            <p>This is a test of Husky</p>
-        </>
-    )
-}
+  return (
+    <>
+      <a href=""></a>
+      <h1>Husky Test</h1>
+      <p>This is a test of Husky</p>
+    </>
+  );
+};
 
-export default huskytest
+export default huskytest;
